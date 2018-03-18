685ecfb24b17213534b1750bafa2c780280ba029
pmosakowski <pmosakowski@users.noreply.github.com>
2018-03-18 17:47:25 +0000

    added OO event loop code

--- gravity/src/gui/mod.rs
+++ gravity/src/gui/mod.rs
@@ -2,6 +2,65 @@ extern crate conrod;
 extern crate glium;
 extern crate find_folder;
 
+use std;
+
+#[allow(dead_code)]
+struct EventLoop<'a> {
+    events_loop: &'a mut glium::glutin::EventsLoop,
+    ui_needs_update: bool,
+    last_update: std::time::Instant,
+}
+
+impl<'a> EventLoop<'a> {
+    pub fn new(events_loop: &'a mut glium::glutin::EventsLoop) -> Self {
+        EventLoop {
+            events_loop: events_loop,
+            last_update: std::time::Instant::now(),
+            ui_needs_update: true,
+        }
+    }
+
+    /// Produce an iterator yielding all available events.
+    pub fn next(&mut self) -> Vec<glium::glutin::Event> {
+        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
+        // since the last yield.
+        let last_update = self.last_update;
+        let sixteen_ms = std::time::Duration::from_millis(16);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        if duration_since_last_update < sixteen_ms {
+            std::thread::sleep(sixteen_ms - duration_since_last_update);
+        }
+
+        // Collect all pending events.
+        let mut events = Vec::new();
+        self.events_loop.poll_events(|event| events.push(event));
+
+        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        if events.is_empty() && !self.ui_needs_update {
+            self.events_loop.run_forever(|event| {
+                events.push(event);
+                glium::glutin::ControlFlow::Break
+            });
+        }
+
+        self.ui_needs_update = false;
+        self.last_update = std::time::Instant::now();
+
+        events
+    }
+
+    /// Notifies the event loop that the `Ui` requires another update whether or not there are any
+    /// pending events.
+    ///
+    /// This is primarily used on the occasion that some part of the `Ui` is still animating and
+    /// requires further updates to do so.
+    pub fn needs_update(&mut self) {
+        self.ui_needs_update = true;
+    }
+
+}
+
+
 #[allow(dead_code)]
 pub struct GliumWindow {
     width: u32,
